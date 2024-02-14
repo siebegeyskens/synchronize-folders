47fa30aabaf7ea4a7b791b7e1beb39d679787f33
Siebe Geyskens <siebe-geyskens@hotmail.com>
2024-02-14 22:20:21 +0000

    use argparser for passing command line variables

---
+++ README.md
@@ -0,0 +1,32 @@
+# Synchronize Folders
+
+This program is designed to synchronize two folders: a source folder and a replica folder. The goal is to ensure that the replica folder maintains an identical copy of the content present in the source folder. This synchronization process is one-way, meaning changes in the source folder will be reflected in the replica folder, but not vice versa.
+
+## Features
+
+- One-way synchronization from source to replica folder.
+- Periodic synchronization at user-defined intervals.
+- Logging of file creation, copying, and removal operations to both console output and a specified log file.
+- Command line arguments for specifying folder paths, synchronization interval, and log file path.
+- Utilizes built-in and external libraries for efficient implementation.
+
+## Requirements
+
+- Python 3.x installed on the system.
+
+## Usage
+
+To use this program, follow these steps:
+
+1. Clone this repository to your local machine.
+2. Navigate to the directory where the program files are located.
+3. Open a terminal or command prompt.
+4. Run the program with the following command:
+
+TODO
+
+## Notes
+
+- Make sure the source folder exist before running the program.
+- If the replica folder doesn't exist yet, it will be created.
+-
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import shutil # For high-level file operations
 import os 
 import sys
 import logging # for logging to a file
+import argparse
 
 def compare(left_directory, right_directory): 
     dircmp_obj = filecmp.dircmp(left_directory, right_directory); # The dircmp object has different methods and attributes to compare the directories that are passed in as arguments
@@ -15,15 +16,15 @@ def compare(left_directory, right_directory):
 
 def copy_files(files):
     for file in files:
-        source_file_path = os.path.join(source_dir, file) # get source file path of the file to copy
-        shutil.copy2(source_file_path, replica_dir) # create and or copy (overwrite) all the files to copy
+        source_file_path = os.path.join(args.source_dir, file) # get source file path of the file to copy
+        shutil.copy2(source_file_path, args.replica_dir) # create and or copy (overwrite) all the files to copy
         log_message = f'{file} has been copied'
         print(log_message)
         logging.info(log_message)
 
 def delete_files(files):
     for file in files: 
-        replica_file_path = os.path.join(replica_dir, file) # get replica file path of the file to delete
+        replica_file_path = os.path.join(args.replica_dir, file) # get replica file path of the file to delete
         os.remove(replica_file_path)
         log_message = f'{file} has been deleted'
         print(log_message)
@@ -35,19 +36,21 @@ def synchronize(source_dir, replica_dir):
     delete_files(files_to_delete) # remove the deleted files 
 
 # Initialize command line arguments
-source_dir = sys.argv[1]; 
-replica_dir = sys.argv[2]; 
-sync_period = int(sys.argv[3]);
-log_file_path = sys.argv[4];
+parser = argparse.ArgumentParser(description='Folder Synchronization')
+parser.add_argument('source_dir', help='Path to the source folder')
+parser.add_argument('--replica_dir', default='replica', help='Path to the replica folder')
+parser.add_argument('--interval', type=int, default=5, help='Synchronization interval in seconds (default: 5)')
+parser.add_argument('--log_file', default='log_file.log', help='Path to the log file (default: log_file.log)')
+args = parser.parse_args()
 
 # Check if replica folder exists. If not, create it.
-if not (os.path.isdir(replica_dir)): 
-    os.mkdir(replica_dir)
+if not (os.path.isdir(args.replica_dir)): 
+    os.mkdir(args.replica_dir)
     print("Created replica_dir")
 
 # setup basic logging to a file
 # specifies the format paramater to record the timestamp of the logged message
-logging.basicConfig(level=logging.INFO, filename=log_file_path, format="%(asctime)s %(levelname)s %(message)s")
+logging.basicConfig(level=logging.INFO, filename=args.log_file, format="%(asctime)s %(levelname)s %(message)s")
 
 # Loop to continuously synchronize folders at a specified time interval
 started = False
@@ -55,5 +58,5 @@ while True:
     if not (started):
         print("Started synchronization loop ...")
         started = True
-    synchronize(source_dir, replica_dir)
-    time.sleep(sync_period) # Sleep for specified time interval
+    synchronize(args.source_dir, args.replica_dir)
+    time.sleep(args.interval) # Sleep for specified time interval
